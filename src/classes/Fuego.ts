5882958ebcfb74f01be8eb1765b309c475520abe
Eric David Smith <erictherobot@gmail.com>
2021-04-17 23:44:53 -0400

    maybe upgrade

--- src/classes/Fuego.ts
+++ src/classes/Fuego.ts
@@ -1,20 +1,21 @@
-import * as firebase from 'firebase/app'
-
-// import 'firebase/firestore'
-// import 'firebase/auth'
-// import 'firebase/functions'
+import { firebase } from '@firebase/app';
+import '@firebase/firestore';
+import '@firebase/auth';
+import '@firebase/storage';
+import '@firebase/analytics';
+import '@firebase/functions';
 
 type Config = Parameters<typeof firebase.initializeApp>[0]
 
 export class Fuego {
-  public db: ReturnType<firebase.app.App['firestore']>
+  public db: ReturnType<any['firestore']>
   public auth: typeof firebase.auth
   public functions: typeof firebase.functions
   public storage: typeof firebase.storage
   constructor(config: Config) {
     this.db = !firebase.apps.length
-      ? firebase.initializeApp(config).firestore()
-      : firebase.app().firestore()
+    ? firebase.initializeApp(config).firestore
+    : firebase.app().firestore;
     this.auth = firebase.auth
     this.functions = firebase.functions
     this.storage = firebase.storage
